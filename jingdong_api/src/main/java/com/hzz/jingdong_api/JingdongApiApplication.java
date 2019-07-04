cf1f5a196d9480fdb5fe5652bd383048f467670f
黄真真 <huangzhenzhen@huangzhenzhendeMacBook-Pro.local>
2019-07-04 10:58:22 +0800

    first-commit

---
+++ jingdong_api/src/main/java/com/hzz/jingdong_api/JingdongApiApplication.java
@@ -0,0 +1,13 @@
+package com.hzz.jingdong_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingdongApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingdongApiApplication.class, args);
+    }
+
+}
